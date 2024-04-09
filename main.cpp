8da47a1ac9ddc4a175adc50cfb9046a1c91ffcdf
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2024-04-09 16:40:18 +0200

    feat: ajout du menu Fichier (sans les fonctions actives)

--- main.cpp
+++ main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 
+#include <QApplication>
+
 #include "model/constante/Constante.h"
 #include "model/operations/addition/Addition.h"
 #include "model/operations/multiplication/Multiplication.h"
+#include "model/fenetre/fenetre.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    Constante c1(5);
-    Constante c2(3);
-    Addition a1(&c1, &c2);
-    Constante c3(2);
-    Multiplication a2(&a1, &c3);
-    a2.afficherNPI();
-    Constante r2 (a2.calculer());
-    std::cout << "\n" ;
-    r2.afficherNPI();
-
-    return 0;
+    QApplication a(argc, argv);
+    FenetrePrincipale w;
+    w.show();
+    return a.exec();
 }
