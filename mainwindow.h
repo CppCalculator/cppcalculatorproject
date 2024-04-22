e80cd4f61377d72a9bafe29a87c6bf9d2205620b
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2024-04-22 10:35:43 +0200

    feat: adding missing menus

---
+++ mainwindow.h
@@ -0,0 +1,63 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QContextMenuEvent>
+#include <QLabel>
+#include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
+#include <QObject>
+#include <QStatusBar>
+#include <QVBoxLayout>
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QActionGroup;
+class QLabel;
+class QMenu;
+QT_END_NAMESPACE
+
+class MainWindow: public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow();
+
+protected:
+#ifndef QT_NO_CONTEXTMENU
+    void contextMenuEvent(QContextMenuEvent *event) override;
+#endif // QT_NO_CONTEXTMENU
+
+private slots:
+    /**
+    void chargerFichier();
+    void enregistrerFichier();
+    void saisirExpression();
+    void affichageClassique();
+    void affichageNPI();
+    void afficherValeurExpression();
+    void affichageGraphique2D();
+    void affichageGraphique3D();
+    void simplificationExpression();
+*/
+
+private:
+    void createActions();
+    void createMenus();
+    QMenu *fichierMenu;
+    QMenu *editionMenu;
+    QMenu *outilsMenu;
+    QAction *chargerFichierAction;
+    QAction *enregistrerFichierAction;
+    QAction *saisirExpressionAction;
+    QAction *affichageClassiqueAction;
+    QAction *affichageNPIAction;
+    QAction *affichageValeurExpressionAction;
+    QAction *affichageGraphique2DAction;
+    QAction *affichageGraphique3DAction;
+    QAction *simplificationExpressionAction;
+    QLabel *infoLabel;
+};
+
+#endif // MAINWINDOW_H
--- model/fenetre/fenetre.cpp
+++ model/fenetre/fenetre.cpp
@@ -36,18 +36,49 @@ FenetrePrincipale::FenetrePrincipale() {
 void FenetrePrincipale::createActions() {
     chargerFichierAction = new QAction(tr("&Ouvrir"), this);
     chargerFichierAction->setShortcut(QKeySequence::Open);
-    chargerFichierAction->setStatusTip(tr("Charger un fichier"));
+    chargerFichierAction->setStatusTip(tr("&Charger un fichier"));
 
     enregistrerFichierAction = new QAction(tr("&Enregistrer"), this);
     enregistrerFichierAction->setShortcut(QKeySequence::Save);
-    enregistrerFichierAction->setStatusTip(tr("Enregistrer dans un fichier"));
+    enregistrerFichierAction->setStatusTip(tr("&Enregistrer dans un fichier"));
 
+    saisirExpressionAction = new QAction(tr("&Saisir une expression"), this);
+    saisirExpressionAction->setStatusTip(tr("&Saisir une expression"));
+
+    affichageClassiqueAction = new QAction(tr("&Afficher l'expression"));
+    affichageClassiqueAction->setStatusTip(tr("&Afficher l'expression"));
+
+    affichageNPIAction = new QAction(tr("&Afficher l'expresion en NPI"));
+    affichageNPIAction->setStatusTip(tr("&Afficher de l'expression en notation Polonaise inversée"));
+
+    affichageValeurExpressionAction = new QAction(tr("&Afficher la valeur de l'expression"));
+    affichageValeurExpressionAction->setStatusTip(tr("&Afficher la valeur de l'expression"));
+
+    affichageGraphique2DAction = new QAction(tr("&Afficher le graphique 2D"));
+    affichageGraphique2DAction->setStatusTip(tr("&Afficher le graphique 2D de l'expression"));
+
+    affichageGraphique3DAction = new QAction(tr("&Affichager le graphique 3D"));
+    affichageGraphique3DAction->setStatusTip(tr("&Afficher le graphique 3D de l'expression"));
+
+    simplificationExpressionAction = new QAction(tr("&Simplier l'expression"));
+    simplificationExpressionAction->setStatusTip(tr("&Simplifier l'expression"));
 }
 
 void FenetrePrincipale::createMenus() {
     fichierMenu = menuBar()->addMenu(tr("&Fichier"));
     fichierMenu->addAction(chargerFichierAction);
     fichierMenu->addAction(enregistrerFichierAction);
+
+    editionMenu = menuBar()->addMenu("&Edition");
+    editionMenu->addAction(saisirExpressionAction);
+    editionMenu->addAction(affichageClassiqueAction);
+    editionMenu->addAction(affichageNPIAction);
+    editionMenu->addAction(affichageValeurExpressionAction);
+    editionMenu->addAction(affichageGraphique2DAction);
+    editionMenu->addAction(affichageGraphique3DAction);
+
+    outilsMenu = menuBar()->addMenu("&Outils");
+    outilsMenu->addAction(simplificationExpressionAction);
 }
 
 #ifndef QT_NO_CONTEXTMENU
