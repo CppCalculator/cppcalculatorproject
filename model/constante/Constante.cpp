550658c8a247dd517f3818780e772f968f8a10ac
zachee-11 <woumbez@gmail.com>
2024-04-28 17:43:45 +0200

    Deuxième itération

---
+++ model/constante/Constante.cpp
@@ -0,0 +1,36 @@
+//
+// Created by mathias.mourgues on 22/03/2024.
+//
+
+#include "Constante.h"
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <string>
+
+void Constante::afficherNC() {
+    std::cout << m_valeur;
+}
+
+void Constante::afficherNPI() {
+    std::cout << m_valeur << " ";
+}
+
+void Constante::afficherNPI(std::ostream& flux) const {
+    flux << m_valeur;
+}
+
+float Constante::calculer() {
+    return m_valeur;
+}
+
+void Constante::sauvegardeASCII( const std::string& n_fichier) {
+    std::ofstream fichier(n_fichier);
+    if (fichier) {
+       afficherNPI(fichier);
+        fichier.close();
+    } else {
+        std::cerr << "Erreur lors de l'ouverture du fichier.\n";
+    }
+}
+
